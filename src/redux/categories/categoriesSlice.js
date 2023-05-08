0e29bd076978cd8f388f211b6df3dcff5d8a44e6
Brian Yrgon <yegonkimutai.99@gmail.com>
2023-05-08 20:49:21 +0300

    Fix linters

--- src/redux/categories/categoriesSlice.js
+++ src/redux/categories/categoriesSlice.js
@@ -1,20 +1,20 @@
 import { createSlice } from '@reduxjs/toolkit';
 
-const initialState =  {
-    categories: [],
-  }
+const initialState = {
+  categories: [],
+};
 
 const categoryList = createSlice({
   name: 'categories',
   initialState,
   reducers: {
     checkStatus: (state, action) => {
-        state.categories = action.payload === 'Under construction'
+      state.categories = action.payload === 'Under construction'
         ? 'Under construction'
-        : state.categories
-    }
-  }
+        : state.categories;
+    },
+  },
 });
 
-export const { checkStatus } = categoryList.actions
+export const { checkStatus } = categoryList.actions;
 export default categoryList.reducer;
