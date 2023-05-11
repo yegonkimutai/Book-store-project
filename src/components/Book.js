cd0bf914907f983295bbfb7c5286b628d8f5ec54
Brian Yrgon <yegonkimutai.99@gmail.com>
2023-05-11 14:01:16 +0300

    Fix linters

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,8 +1,8 @@
 import { useState } from 'react';
 import { PropTypes } from 'prop-types';
 import { useDispatch } from 'react-redux';
+import { Circle } from 'rc-progress';
 import { removeABook } from '../redux/books/booksSlice';
-import { Circle } from 'rc-progress'; 
 
 import './Book.css';
 
@@ -10,41 +10,47 @@ const Book = ({
   id, title, author,
 }) => {
   const dispatch = useDispatch();
-  const [percentage, setPercentage] = useState(30)
+  const [percentage, setPercentage] = useState(30);
 
   return (
-    <div className='whole-book'>
-     <div className="single-book" key={id}>
-      <h2>{title}</h2>
-      <p>{author}</p>
+    <div className="whole-book">
+      <div className="single-book" key={id}>
+        <h2>{title}</h2>
+        <p>{author}</p>
 
-      <button type='button' className='btn2'>Comment</button>
-      <button
-        type="button"
-        onClick={() => dispatch(removeABook(id))}
-      >
-        Remove
-      </button>
-      <button type='button' className='btn3'>Edit</button>
+        <button type="button" className="btn2">Comment</button>
+        <button
+          type="button"
+          onClick={() => dispatch(removeABook(id))}
+        >
+          Remove
+        </button>
+        <button type="button" className="btn3">Edit</button>
+      </div>
+      <div className="progress">
+        <Circle
+          className="prog-img"
+          percent={percentage}
+          strokeColor="blue"
+          strokeWidth={5}
+          trailWidth={5}
+        />
+        <p className="text">
+          <span className="figure">
+            {percentage}
+            %
+          </span>
+          <br />
+          Completed
+        </p>
+      </div>
+      <div className="display">
+        <h2>CURRENT CHAPTER</h2>
+        <p>Chapter 3</p>
+        <button type="button" onClick={() => setPercentage(percentage + 1)}>UPDATE PROGRESS</button>
+      </div>
     </div>
-    <div className='progress'>
-      <Circle className='prog-img'
-      percent={percentage}
-      strokeColor='blue'
-      strokeWidth={5} 
-      trailWidth={5}/>
-      <p className='text'>
-        <span className='figure'>{percentage}%</span><br></br>
-        Completed
-      </p>
-    </div>
-    <div className='display'>
-      <h2>CURRENT CHAPTER</h2>
-      <p>Chapter 3</p>
-      <button type='button' onClick={() => setPercentage(percentage + 1)}>UPDATE PROGRESS</button>
-    </div>
-    </div>
-   
+
   );
 };
 
